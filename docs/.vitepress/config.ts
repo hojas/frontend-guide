ee0503e6a8cedec243ca1d6f5cbc16c9a3da36e5
hojas <cyxhuan9@163.com>
2022-06-14 21:41:56 +0800

    add pages

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -14,39 +14,39 @@ export default {
       {
         text: '前端基础',
         items: [
-          { text: 'HTML', link: '/html/' },
-          { text: 'CSS', link: '/css/' },
-          { text: 'JavaScript', link: '/javascript/' },
-          { text: 'TypeScript', link: '/typescript/' },
+          { text: 'HTML', link: '/html' },
+          { text: 'CSS', link: '/css' },
+          { text: 'JavaScript', link: '/javascript' },
+          { text: 'TypeScript', link: '/typescript' },
         ],
       },
       {
         text: '前端框架',
         items: [
-          { text: 'Vue', link: '/vue/' },
-          { text: 'React', link: '/react/' },
-          { text: 'Angular', link: '/angular/' },
+          { text: 'Vue', link: '/vue' },
+          { text: 'React', link: '/react' },
+          { text: 'Angular', link: '/angular' },
         ],
       },
       {
         text: '前端工程化',
-        link: '/engineering/',
+        link: '/engineering',
       },
       {
         text: '性能优化',
-        link: '/performance/',
+        link: '/performance',
       },
       {
         text: '浏览器原理',
-        link: '/browser/',
+        link: '/browser',
       },
       {
         text: '移动端',
-        link: '/mobile/',
+        link: '/mobile',
       },
       {
         text: 'Node.js',
-        link: '/nodejs/',
+        link: '/nodejs',
       },
     ],
     socialLinks: [{ icon: 'github', link: 'https://github.com/hojas' }],
