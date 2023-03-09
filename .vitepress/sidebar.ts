0c2664d1b97cba84eb9f1046158ac7eb5052f0be
hojas <cyxhuan9@163.com>
2023-03-09 17:23:33 +0800

    update

--- .vitepress/sidebar.ts
+++ .vitepress/sidebar.ts
@@ -5,9 +5,9 @@ export const sidebar: DefaultTheme.Sidebar = {
     {
       text: '浏览器原理',
       items: [
-        { text: '事件循环', link: '/browser/event-loop.html' },
         { text: 'blink 渲染引擎', link: '/browser/blink.html' },
         { text: 'v8 引擎简介', link: '/browser/v8.html' },
+        { text: '事件循环', link: '/browser/event-loop.html' },
       ],
     },
   ],
