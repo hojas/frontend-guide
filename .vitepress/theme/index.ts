976349be5c8081e23ae9edd66dc60219832c8102
hojas <cyxhuan9@163.com>
2023-12-08 15:00:12 +0800

    add Giscus

---
+++ .vitepress/theme/index.ts
@@ -0,0 +1,8 @@
+import type { Theme } from 'vitepress'
+import DefaultTheme from 'vitepress/theme'
+import MyLayout from './MyLayout.vue'
+
+export default {
+  extends: DefaultTheme,
+  Layout: MyLayout,
+} satisfies Theme
