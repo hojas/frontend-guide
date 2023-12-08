19fa3d2465942062e7b16124018a6823006e95a1
hojas <cyxhuan9@163.com>
2023-12-08 15:07:08 +0800

    add about page

--- .vitepress/nav.ts
+++ .vitepress/nav.ts
@@ -2,7 +2,7 @@ import type { DefaultTheme } from 'vitepress'
 
 export const nav: DefaultTheme.NavItem[] = [
   { text: '首页', link: '/' },
-  { text: '浏览器原理', link: '/browser' },
+  { text: '浏览器原理', link: '/browser.html' },
   // { text: '前端框架', link: '/frameworks' },
   // { text: '前端工程化', link: '/engineering' },
   // { text: '前端测试', link: '/testing' },
@@ -22,5 +22,9 @@ export const nav: DefaultTheme.NavItem[] = [
   {
     text: '算法',
     link: 'https://www.programmercarl.com/',
+  },
+  {
+    text: '关于',
+    link: '/about.html',
   },
 ]
--- src/docs/about.md
+++ src/docs/about.md
@@ -4,7 +4,5 @@ title: 关于我
 
 # 关于我
 
-热爱 Web 前端开发，本站主要收集整理网络上前端开发相关的优质内容，希望能够系统地完善前端开发知识。
-
 - Github: https://github.com/hojas
 - Email: cyxhuan9#163.com
