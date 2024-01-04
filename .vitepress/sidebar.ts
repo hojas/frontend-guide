6be2c102e59c9d0ca70ad3406504a260f1726e79
hojas <cyxhuan9@163.com>
2024-01-04 19:54:34 +0800

    update config

--- .vitepress/sidebar.ts
+++ .vitepress/sidebar.ts
@@ -5,7 +5,7 @@ export const sidebar: DefaultTheme.Sidebar = {
     {
       text: 'ECMAScript',
       items: [
-        { text: '2023', link: '/ecmascript/2023.html' },
+        { text: '2023', link: '/ecmascript/2023' },
       ],
     },
   ],
@@ -13,8 +13,8 @@ export const sidebar: DefaultTheme.Sidebar = {
     {
       text: 'JavaScript',
       items: [
-        { text: '数据类型', link: '/javascript/type.html' },
-        { text: 'async/await的原理', link: '/javascript/async-await.html' },
+        { text: '数据类型', link: '/javascript/type' },
+        { text: 'async/await的原理', link: '/javascript/async-await' },
       ],
     },
   ],
@@ -22,7 +22,7 @@ export const sidebar: DefaultTheme.Sidebar = {
     {
       text: 'TypeScript',
       items: [
-        { text: '工具类型', link: '/typescript/utility-types.html' },
+        { text: '工具类型', link: '/typescript/utility-types' },
       ],
     },
   ],
@@ -30,9 +30,9 @@ export const sidebar: DefaultTheme.Sidebar = {
     {
       text: 'Vue',
       items: [
-        { text: '响应式原理', link: '/vue/reactivity.html' },
-        { text: 'Diff 算法', link: '/vue/diff.html' },
-        // { text: '路由', link: '/vue/router.html' },
+        { text: '响应式原理', link: '/vue/reactivity' },
+        { text: 'Diff 算法', link: '/vue/diff' },
+        // { text: '路由', link: '/vue/router' },
       ],
     },
   ],
@@ -40,9 +40,9 @@ export const sidebar: DefaultTheme.Sidebar = {
     {
       text: '浏览器原理',
       items: [
-        { text: '进程和线程', link: '/browser/process-and-thread.html' },
-        { text: '事件循环', link: '/browser/event-loop.html' },
-        { text: '页面渲染原理', link: '/browser/rendering.html' },
+        { text: '进程和线程', link: '/browser/process-and-thread' },
+        { text: '事件循环', link: '/browser/event-loop' },
+        { text: '页面渲染原理', link: '/browser/rendering' },
       ],
     },
   ],
@@ -50,8 +50,8 @@ export const sidebar: DefaultTheme.Sidebar = {
     {
       text: '手搓代码',
       items: [
-        { text: '音频可视化的实现', link: '/source-code/voice-visualization.html' },
-        { text: '深拷贝', link: '/source-code/deep-copy.html' },
+        { text: '音频可视化的实现', link: '/source-code/voice-visualization' },
+        { text: '深拷贝', link: '/source-code/deep-copy' },
       ],
     },
   ],
