36e168e2b8aebff6543d680bbb9a09dab2c646dd
陈远翔 <cyxhuan9@163.com>
2024-09-28 16:51:06 +0800

    update

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,15 +1,20 @@
 import { defineConfig } from 'vitepress'
+import { head } from './head'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  lang: 'zh-Hans',
   title: '陈远翔',
   description: '热爱前端开发',
+  ignoreDeadLinks: true,
+  head,
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: '首页', link: '/' },
       { text: '博客', link: '/blog' },
       { text: '项目', link: '/project' },
+      { text: '关于', link: '/about' },
     ],
 
     sidebar: {
@@ -33,11 +38,20 @@ export default defineConfig({
           ],
         },
       ],
+    },
 
+    docFooter: {
+      prev: '上一篇',
+      next: '下一篇',
     },
 
     socialLinks: [
       { icon: 'github', link: 'https://github.com/hojas' },
     ],
+
+    footer: {
+      message: '陈远翔个人网站',
+      copyright: 'Copyright © 2024-present feguide.net',
+    },
   },
 })
--- docs/about.md
+++ docs/about.md
@@ -1,3 +1,19 @@
 # 关于我
 
-Email: chenyuanxiang.x(at)gmail.com
+前端开发一枚。
+
+## 技术
+
+1. 熟悉 JavaScript/ES6、TypeScript 等开发语言
+2. 熟悉 Vue3 及其实现原理，熟悉 Nuxt.js 开发
+3. 了解 React 和 Next.js 技术栈开发
+4. 熟悉 webpack、vite、rollup 等打包构建工具
+5. 熟悉 ESLint、Prettier、StyleLint 等质量工具
+6. 熟悉 Babel、PostCSS、SASS、TailwindCSS 等技术
+7. 熟悉 Git、Linux、Docker、Sentry 等工具
+8. 了解 Python、Rust 等语言
+
+## 联系方式
+
+- Github: https://github.com/hojas
+- Email: chenyuanxiang.x@gmail.com
