711865a44dfe1ff6832aa21fa243b25234f586db
陈远翔 <cyxhuan9@163.com>
2024-11-01 16:03:43 +0800

    update docs

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vitepress'
 import { head } from './head'
+import { sidebar } from './sidebar'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
@@ -17,28 +18,7 @@ export default defineConfig({
       { text: '关于', link: '/about' },
     ],
 
-    sidebar: {
-      '/blog/': [
-        {
-          text: '博客',
-          items: [
-            { text: '浏览器页面渲染原理', link: '/blog/6' },
-            { text: '浏览器事件循环原理', link: '/blog/5' },
-            { text: '手写实现数组实例方法', link: '/blog/4' },
-            { text: '使用 Docker 一键部署带 Let\'s Encrypt SSL 证书的 Nginx', link: '/blog/3' },
-            { text: '在 Vue 中实现音频可视化', link: '/blog/2' },
-            { text: 'JavaScript 脚本的加载时机', link: '/blog/1' },
-          ],
-        },
-      ],
-      '/project/': [
-        {
-          text: '项目',
-          items: [
-          ],
-        },
-      ],
-    },
+    sidebar,
 
     docFooter: {
       prev: '上一篇',
---
+++ docs/.vitepress/sidebar.ts
@@ -0,0 +1,23 @@
+export const sidebar = {
+  '/blog/': [
+    {
+      text: '博客',
+      items: [
+        { text: 'Vue3 的响应式原理简介', link: '/blog/7' },
+        { text: '浏览器页面渲染原理', link: '/blog/6' },
+        { text: '浏览器事件循环原理', link: '/blog/5' },
+        { text: '手写实现数组实例方法', link: '/blog/4' },
+        { text: '使用 Docker 一键部署带 Let\'s Encrypt SSL 证书的 Nginx', link: '/blog/3' },
+        { text: '在 Vue 中实现音频可视化', link: '/blog/2' },
+        { text: 'JavaScript 脚本的加载时机', link: '/blog/1' },
+      ],
+    },
+  ],
+  '/project/': [
+    {
+      text: '项目',
+      items: [
+      ],
+    },
+  ],
+}
